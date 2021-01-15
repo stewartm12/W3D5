57bf9198e881d8eb48d982a791949b71a6e6b46f
stewart morales <stewartmorales129@gmail.com>
2021-01-15 13:55:06 -0500

    Define PolyTree#Initialize

--- Node_project/skeleton/lib/00_tree_node.rb
+++ Node_project/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value 
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    @parent = new_parent
+    unless @parent == nil
+      @parent.children << self.value 
+    end
+  end
+
 
 end
